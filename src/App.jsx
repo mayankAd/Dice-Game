5e8b6db57b2b1ed05af51e8cfaab8161df733890
Mayank <97655679+mayankAd@users.noreply.github.com>
2023-08-10 21:41:59 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: black;
+`;
+
+function App() {
+  return (
+    <>
+      <Button>This is styled component</Button>
+    </>
+  );
+}
+export default App;
