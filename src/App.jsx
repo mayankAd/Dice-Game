9f3dded0d1b8b726779f6106dc63facef1e8de88
Mayank <97655679+mayankAd@users.noreply.github.com>
2023-08-12 21:17:33 +0530

    Completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import styled from "styled-components";
-
-const Button = styled.button`
-  background-color: black;
-`;
+import StartGame from "./components/StartGame";
+import Gameplay from "./components/Gameplay";
+import { useState } from "react";
 
 function App() {
+  const [isGameStarted, setIsGameStarted] = useState(true);
+
+  const toggleGamePlay = () => {
+    setIsGameStarted((prev) => !prev);
+  };
+
   return (
-    <>
-      <Button>This is styled component</Button>
-    </>
+    <>{isGameStarted ? <Gameplay /> : <StartGame toggle={toggleGamePlay} />}</>
   );
 }
 export default App;
